7d13361feec0e04ca1ca4be603c09e48f2044162
David Svensson <davidsvson@gmail.com>
2021-09-26 21:53:58 +0200

    Update WordList.kt

--- app/src/main/java/com/example/flashcard/WordList.kt
+++ app/src/main/java/com/example/flashcard/WordList.kt
@@ -2,79 +2,56 @@ package com.example.flashcard
 
 import android.util.Log
 
-class WordList (){
-    private val wordList = mutableListOf<Word>()
-   // private val usedWords = mutableListOf<Word>()
-   // private var nrOfWordsUsed = 0
+
+class WordList() {
+    private val words = mutableListOf<Word>()
+    private var currentWord : Word? = null
+    private var originalDirection = true
 
     init {
-        initializeWords()
+        initiliaze()
     }
 
-    private fun initializeWords() {
-        val word = Word("Hello", "Hej")
-        wordList.add(word)
-        wordList.add(Word("Good bye", "Hej då"))
-        wordList.add(Word("Thank you", "Tack"))
-        wordList.add(Word("Welcome", "Välkommen"))
-        wordList.add(Word("Computer", "Dator"))
+    fun initiliaze() {
+        words.add(Word("Hej", "Hello"))
+        words.add(Word("Hej då", "Good bye"))
+        words.add(Word("Tack", "Thank you"))
+    }
+        
+    private fun getNewWord() : Word {
+        val rnd = (0 until words.size).random()
+        val word = words.removeAt(rnd)
+        return word
     }
 
-    // alternativ 2 -
-//    fun getNewWord() : Word {
-//        if (nrOfWordsUsed >= wordList.size) {
-//            nrOfWordsUsed = 0
-//            for(word in wordList) {
-//                word.used = false
-//            }
-//        }
-//
-//        var newWord : Word? = null
-//
-//        do {
-//            val rnd = (0 until wordList.size).random()
-//            newWord = wordList[rnd]
-//        } while(newWord!!.used )
-//
-//        newWord.used = true
-//        nrOfWordsUsed++
-//
-//        return newWord
-//    }
-
-
-
-    // alternativ 3
-    fun getNewWord() : Word {
-        if (wordList.isEmpty()) {
-            initializeWords()
+    fun changeWord() {
+        if (words.isEmpty()) {
+            initiliaze()
         }
-        val rnd = (0 until wordList.size).random()
+        val rnd = (0 until words.size).random()
+        currentWord = words.removeAt(rnd)
+    }
 
-        val newWord = wordList.removeAt(rnd)
-        return newWord
+    fun revertTranslateDirection() {
+        originalDirection = !originalDirection
+    }
+
+    fun getOriginalWord() : String? {
+        return if (originalDirection) {
+            currentWord?.swedish
+        } else {
+            currentWord?.english
+        }
     }
 
 
+    fun getTranslatedWord() : String? {
+        return if (!originalDirection) {
+            currentWord?.swedish
+        } else {
+            currentWord?.english
+        }
+    }
 
-    // alternativ 4
-//    fun getNewWord() : Word {
-//        if (wordList.size == 0) {
-//           wordList.addAll(usedWords)
-//           usedWords.clear()
-//        }
-//
-//        val rnd = (0 until wordList.size).random()
-//
-//        val newWord = wordList.removeAt(rnd)
-//        usedWords.add(newWord)
-//        return newWord
-//    }
+
 }
-
-// 1. list av använda ord
-// 4st  0st 2. en bool i word-klassen som håller reda på om ordet är använt
-// 4st  7st  3. ta bort det använda ordet ur listan
-
-// 3st 6st 4. två stycken listor, flytta använda ord till ny lista
-// 5. ta orden i ordning från 0 till så många ord vi har
